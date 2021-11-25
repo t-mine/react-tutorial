dc2ae72360986358c99c131efb3305f04e330ddc
Tomonori Mine <tomonori.mine.biz@gmail.com>
2021-11-25 21:12:57 +0900

    Board でマス目を並べる部分を、ハードコーディングではなく 2 つのループを使用するように書き換える。

--- src/index.jsx
+++ src/index.jsx
@@ -16,28 +16,26 @@ class Board extends React.Component {
       <Square
         value={this.props.squares[i]} //
         onClick={() => this.props.onClick(i)}
+        key={i}
       />
     );
   }
 
   render() {
+    const boardArray = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+    ];
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {boardArray.map((row, rowIdx) => {
+          return (
+            <div className="board-row" key={rowIdx}>
+              {row.map((value) => this.renderSquare(value))}
+            </div>
+          );
+        })}
       </div>
     );
   }
